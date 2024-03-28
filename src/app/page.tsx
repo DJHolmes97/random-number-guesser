64b08b4cd8857f07074afac0767f5b14bf924249
Daniel Holmes <daniel.holmes97@hotmail.com>
2024-03-28 09:28:22 +0000

    Made chat input sticky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import GameContainer from "@/containers/GameContainer/GameContainer"
 
 export default function Home() {
   return (
-    <main className="flex flex-grow flex-shrink-0 h-full flex-col items-center md:px-24 px-2">
+    <main className="flex flex-grow flex-shrink-0 h-full flex-col items-center">
       <Header />
       <GameContainer />
     </main>
