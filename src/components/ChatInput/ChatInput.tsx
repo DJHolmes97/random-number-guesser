c6e7cb8284c29298b2afba6a1aca82ad6fe52b23
Daniel Holmes <daniel.holmes97@hotmail.com>
2024-04-02 14:10:37 +0100

    add icon to status to improve how it looks

--- src/components/ChatInput/ChatInput.tsx
+++ src/components/ChatInput/ChatInput.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { ChangeEvent } from "react"
 import "./main.css"
+import * as icon from "./circle-exclamation-solid.svg"
 
 type ChatInputProps = {
   value: string
@@ -9,8 +10,23 @@ type ChatInputProps = {
   status?: string
 }
 
+const Icon = () => {
+  return (
+    <div className="chat-input-icon">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
+      </svg>
+    </div>
+  )
+}
+
 const Status = ({ message }: { message: string }) => {
-  return <div className="chat-input-status">{message}</div>
+  return (
+    <div className="chat-input-status">
+      <Icon />
+      {message}
+    </div>
+  )
 }
 
 const ChatInput = ({ value, onChange, onSubmit, status }: ChatInputProps) => {
