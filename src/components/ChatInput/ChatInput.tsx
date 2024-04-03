5390d8eb526be0c55cbd442a4a28420d118c5768
Daniel Holmes <daniel.holmes97@hotmail.com>
2024-04-03 14:22:58 +0100

    added colour to status icon and fixed mobile styling

--- src/components/ChatInput/ChatInput.tsx
+++ src/components/ChatInput/ChatInput.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { ChangeEvent } from "react"
 import "./main.css"
-import * as icon from "./circle-exclamation-solid.svg"
 
 type ChatInputProps = {
   value: string
@@ -14,7 +13,10 @@ const Icon = () => {
   return (
     <div className="chat-input-icon">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-        <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
+        <path
+          d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"
+          fill="currentColor"
+        />
       </svg>
     </div>
   )
